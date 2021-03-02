5d0bce1d1168e13556c7f20130948afb5b7786ba
duykypaul <duykypaul@gmail.com>
2021-03-02 10:27:15 +0700

    add input content[test case]

--- src/com/duykypaul/kltn/ARNV3.java
+++ src/com/duykypaul/kltn/ARNV3.java
@@ -10,22 +10,23 @@ public class ARNV3 {
     private static final double MUTATION_RATE =  0.01;
     private static final double CROSSOVER_RATE = 0.95;
     private static final int ELITISM_COUNT =  3;
-    private static final int RUNNING_TIME_LIMIT = 3;
+    private static final int RUNNING_TIME_LIMIT = 200;
     private static final int GENERATION_LIMIT = 1;
-
-    private static final String COMMA = ",";
 
     public static void main(String[] args) {
         List<Stack> listStack = new ArrayList<>();
-        listStack.add(new Stack(2, 3000, LocalDate.parse("2021-02-28")));
-        listStack.add(new Stack(3, 2000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(10, 3000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(20, 2000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(30, 5000, LocalDate.parse("2021-02-28")));
 
-        listStack.add(new Stack(3, 5000, LocalDate.parse("2021-03-05")));
-        listStack.add(new Stack(4, 7000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(40, 5000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(50, 7000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(20, 3000, LocalDate.parse("2021-03-05")));
 
         List<Stock> listStock = new ArrayList<>();
-        listStock.add(new Stock(20, 13000, LocalDate.parse("2021-02-12")));
+        listStock.add(new Stock(200, 13000, LocalDate.parse("2021-02-12")));
         listStock.add(new Stock(15, 8000, LocalDate.parse("2021-02-15")));
+        listStock.add(new Stock(30, 7995, LocalDate.parse("2021-02-15")));
 
         final List<Integer> orders = new ArrayList<>();
         final List<LocalDate> ordersDate = new ArrayList<>();
@@ -63,11 +64,11 @@ public class ARNV3 {
         ga.evalPopulation(population);
         int generation = 1;
 
-        Set<String> stringSet = new HashSet<>();
+        Set<Double> stringSet = new HashSet<>();
 
         while (!ga.isTerminationConditionMet(population, start)) {
             // Print fittest individual from population
-            stringSet.add(population.getFittest(0).toString());
+            stringSet.add(population.getFittest(0).getFitness());
             System.out.println("Best solution: " + population.getFittest(0).toString());
             System.out.println("Best value: " + population.getFittest(0).getFitness());
             System.out.println();
@@ -88,7 +89,7 @@ public class ARNV3 {
         System.out.println("Found solution in " + generation + " generations");
         System.out.println("Best solution final: " + population.getFittest(0).toString());
         System.out.println("Best value final: " + population.getFittest(0).getFitness());
-        stringSet.add(population.getFittest(0).toString());
+        stringSet.add(population.getFittest(0).getFitness());
         System.out.println("set size: " + stringSet.size());
 
         Instant end = Instant.now();
