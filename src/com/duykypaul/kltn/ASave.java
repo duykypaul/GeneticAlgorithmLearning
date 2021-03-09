4bfac369b12d52ea6360c189ffcc8a1fde9f8f21
duykypaul <duykypaul@gmail.com>
2021-03-10 00:49:08 +0700

    Statistic info of fittest individual from population[KLTN]

--- src/com/duykypaul/kltn/ASave.java
+++ src/com/duykypaul/kltn/ASave.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ASave {
@@ -14,18 +15,18 @@ public class ASave {
     private static final double MUTATION_RATE = 0.01;
     private static final double CROSSOVER_RATE = 0.95;
     private static final int ELITISM_COUNT = 3;
-    private static final int RUNNING_TIME_LIMIT = 200;
+    private static final int RUNNING_TIME_LIMIT = 30;
     private static final int GENERATION_LIMIT = 1;
 
     public static void main(String[] args) {
         List<Stack> listStack = new ArrayList<>();
-        listStack.add(new Stack(10, 3000, LocalDate.parse("2021-02-28")));
-        listStack.add(new Stack(20, 2000, LocalDate.parse("2021-02-28")));
-        listStack.add(new Stack(30, 5000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(1, 1, 10, 3000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(1, 2, 20, 2000, LocalDate.parse("2021-02-28")));
+        listStack.add(new Stack(1, 3, 30, 5000, LocalDate.parse("2021-02-28")));
 
-        listStack.add(new Stack(40, 5000, LocalDate.parse("2021-03-05")));
-        listStack.add(new Stack(50, 7000, LocalDate.parse("2021-03-05")));
-        listStack.add(new Stack(20, 3000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(2, 1, 40, 5000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(2, 2, 50, 7000, LocalDate.parse("2021-03-05")));
+        listStack.add(new Stack(2, 3, 20, 3000, LocalDate.parse("2021-03-05")));
 
         List<Stock> listStock = new ArrayList<>();
         listStock.add(new Stock(200, 13000, LocalDate.parse("2021-02-12")));
@@ -86,17 +87,22 @@ public class ASave {
             generation++;
         }
 
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end));
+        System.out.println();
         System.out.println("Found solution in " + generation + " generations");
         outputReport(population);
         System.out.println("Number of kings: " + resultSet.size());
-        Instant end = Instant.now();
-        System.out.println(Duration.between(start, end));
+        System.out.println();
+
+        outputStatistic(population, machines, listStack);
+
     }
 
     /**
      * Print fittest individual from population
      *
-     * @param population
+     * @param population p
      */
     private static void outputReport(Population population) {
         Individual best = population.getFittest(0);
@@ -104,6 +110,30 @@ public class ASave {
         System.out.println("Best solution machines index: " + best.getChromosomeMachine().stream().map(String::valueOf).collect(Collectors.joining(",")));
         System.out.println("Best solution datetime cut product: " + best.getChromosomeTime().stream().map(String::valueOf).collect(Collectors.joining(",")));
         System.out.println("Best value remain: " + best.getFitness());
+        System.out.println();
+    }
+
+    /**
+     * Print Statistic info of fittest individual from population
+     *
+     * @param population p
+     */
+    private static void outputStatistic(Population population, List<Machine> machines, List<Stack> stacks) {
+        Individual best = population.getFittest(0);
+        AtomicInteger indexBeginStack = new AtomicInteger();
+        stacks.forEach(stack -> {
+            System.out.println("Item: " + stack.getIndexStack() + " -- Consignment: " + stack.getIndexConsign() + " -- Length: " + stack.getLength());
+            List<Integer> subListIndexMachine = best.getChromosomeMachine().subList(indexBeginStack.get(), indexBeginStack.get() + stack.getQuantity());
+            List<String> subListIndexTime = best.getChromosomeTime().subList(indexBeginStack.get(), indexBeginStack.get() + stack.getQuantity());
+            for (int indexMachine = 0; indexMachine < machines.size(); indexMachine++) {
+                int finalIndexMachine = indexMachine;
+                long mount = subListIndexMachine.stream().filter(item -> item.equals(finalIndexMachine)).count();
+                System.out.println("Machine " + (indexMachine + 1) + " cuts " + mount + "/" + stack.getQuantity()
+                    + " of this, starting from the moment " + subListIndexTime.get(subListIndexMachine.indexOf(finalIndexMachine)));
+            }
+            System.out.println("====================================================================");
+            indexBeginStack.addAndGet(stack.getQuantity());
+        });
         System.out.println();
     }
 }
--- src/com/duykypaul/kltn/Stack.java
+++ src/com/duykypaul/kltn/Stack.java
@@ -3,14 +3,34 @@ package com.duykypaul.kltn;
 import java.time.LocalDate;
 
 public class Stack {
+    private int indexConsign;
+    private int indexStack;
     private int quantity;
     private int length;
     private LocalDate deliveryDate;
 
-    public Stack(int quantity, int length, LocalDate deliveryDate) {
+    public Stack(int indexConsign, int indexStack, int quantity, int length, LocalDate deliveryDate) {
+        this.indexConsign = indexConsign;
+        this.indexStack = indexStack;
         this.quantity = quantity;
         this.length = length;
         this.deliveryDate = deliveryDate;
+    }
+
+    public int getIndexConsign() {
+        return indexConsign;
+    }
+
+    public void setIndexConsign(int indexConsign) {
+        this.indexConsign = indexConsign;
+    }
+
+    public int getIndexStack() {
+        return indexStack;
+    }
+
+    public void setIndexStack(int indexStack) {
+        this.indexStack = indexStack;
     }
 
     public int getQuantity() {
