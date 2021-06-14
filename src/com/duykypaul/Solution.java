37564710593287a4a8b330984f683297b2e6bf87
duykypaul <duykypaul@gmail.com>
2021-06-15 00:38:33 +0700

    update report and presentation

--- src/com/duykypaul/Solution.java
+++ src/com/duykypaul/Solution.java
@@ -1,59 +1,40 @@
 package com.duykypaul;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+
+enum Kikaku {
+    a,
+    b,
+    c,
+    d
+}
 
 class Solution {
     public static void main(String[] args) {
-//        int[][] arr = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
-//        int[][] arr = {{1, 0}};
-        int[][] arr = {{2, 0}, {2, 1}};
-        int k = 3;
-        System.out.println(arr.length);
-        System.out.println(Arrays.toString(new Solution().findOrder(k, arr)));
-    }
+        List<String> strings = Arrays.asList("a", "b", "c", "d", "g");
+        String a = Math.random() > 0.3 ? Kikaku.a.name() : "";
+        String b = Math.random() > 0.3 ? Kikaku.b.name() : "";
+        String c = Math.random() > 0.3 ? Kikaku.c.name() : "";
+        String d = Math.random() > 0.3 ? Kikaku.d.name() : "";
+        String other = Math.random() > 0.3 ? "o" : "";
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+        System.out.println("other: " + other);
+        List<String> conditions = Arrays.asList(a, b, c, d);
+        Predicate<String> filter = x -> {
+            boolean checkMain = conditions.stream().anyMatch(item -> !item.isEmpty() && x.startsWith(item));
+            if (checkMain) return true;
+            if (!other.isEmpty()) {
+                boolean checkOther = Arrays.stream(Kikaku.values()).noneMatch(item -> x.startsWith(item.name()));
+                if(checkOther) return true;
+            }
+            return (conditions.stream().allMatch(String::isEmpty) && other.isEmpty() || conditions.stream().noneMatch(String::isEmpty) && !other.isEmpty());
+        };
 
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] arr = new int[numCourses];
-        int count = 0;
-        if (prerequisites.length == 0) {
-            for (int i = 0; i < numCourses; i++) {
-                arr[i] = i;
-            }
-            return arr;
-        }
-        List<Integer> lowLevel = new ArrayList<>();
-
-        for (int[] item : prerequisites) {
-            if (lowLevel.contains(item[1]) && lowLevel.contains(item[0]) && lowLevel.indexOf(item[0]) < lowLevel.indexOf(item[1])) {
-                return new int[]{};
-            }
-
-            if (!lowLevel.contains(item[1])) {
-                if(!lowLevel.contains(item[0])) {
-                    if (count == numCourses) {
-                        return lowLevel.stream().mapToInt(Integer::intValue).toArray();
-                    } else {
-                        count++;
-                        lowLevel.add(item[1]);
-                    }
-                } else {
-                    int indexFirstItem = lowLevel.indexOf(item[0]);
-                    count++;
-                    lowLevel.add(indexFirstItem, item[1]);
-                }
-            }
-            if (count == numCourses) {
-                return lowLevel.stream().mapToInt(Integer::intValue).toArray();
-            } else {
-                count++;
-                lowLevel.add(item[0]);
-            }
-        }
-        for (int i = count; i < numCourses; i++) {
-            lowLevel.add(i);
-        }
-        return lowLevel.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(Arrays.toString(strings.stream().filter(filter).toArray()));
     }
 }
